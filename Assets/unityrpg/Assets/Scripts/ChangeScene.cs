12979a20822358be58987fe34ffdd045af970fc6
ayham abuarida <ayham88934@gmail.com>
2022-06-20 20:36:45 +0300

    final

---
+++ Assets/unityrpg/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void loadNextScene(string Town) {
+		SceneManager.LoadScene (Town);
+	}
+}
